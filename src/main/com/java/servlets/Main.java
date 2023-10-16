c8cea9ec90b5bbfe4a965736c02c14640d418b04
Akmal Husanov <a.khusanov@gmail.com>
2023-10-16 18:05:51 -0400

    add doGet method

--- src/main/com/java/servlets/Main.java
+++ src/main/com/java/servlets/Main.java
@@ -50,4 +50,37 @@ public class Main extends HttpServlet {
             e.printStackTrace();
         }
     }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        try {
+            String firstName = request.getParameter("firstname");
+            String lastName = request.getParameter("lastname");
+            String gender = request.getParameter("gender");
+            System.out.println("first name: " + firstName);
+            System.out.println("last name: " + lastName);
+            System.out.println("gender: " + gender);
+
+            if (firstName == null || lastName == null) {
+                out.println("<h1>Error: Please enter both first name and last name.</h1>");
+            } else if (gender == null) {
+                out.println("<h1>Error: Please select a gender.</h1>");
+            } else {
+                out.println("<h1>Welcome to my Shop</h1>");
+                if (gender.equals("Male")) {
+                    out.println("<p>Thank you Mr. " + firstName + " " + lastName + ".</p>");
+                } else if (gender.equals("Female")) {
+                    out.println("<p>Thank you Ms. " + firstName + " " + lastName + ".</p>");
+                } else {
+                    out.println("<p>Thank you " + firstName + " " + lastName + ".</p>");
+                }
+            }
+        } catch (Exception e) {
+            // Handle the exception, e.g., log it or display an error message
+            out.println("<h1>Error: An unexpected error occurred.</h1>");
+            e.printStackTrace();
+        }
+    }
 }
